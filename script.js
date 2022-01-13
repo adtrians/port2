0c5ec75a8b786b74a3dd8721dbee960b6b8b3bf3
adtrians <85712536+adtrians@users.noreply.github.com>
2022-01-13 11:10:49 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,142 +1,34 @@
-window.addEventListener("load", event => {
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
 
-    const videos = [{
-            name: "Showreel",
-            img: "img/1.jpeg",
-            video: "https://www.youtube.com/embed/H2Ay1vC9sEQ"
-        },
-        
-        {
-            name: "Showreel",
-            img: "img/2.jpeg",
-            video: "https://www.youtube.com/embed/CcFYzYj0xHw"
-        },
+// Select tab content item
+function selectItem(e) {
+	// Remove all show and border classes
+	removeBorder();
+	removeShow();
+	// Add border to current tab item
+	this.classList.add('tab-border');
+	// Grab content item from DOM
+	const tabContentItem = document.querySelector(`#${this.id}-content`);
+	// Add show class
+	tabContentItem.classList.add('show');
+}
 
-        {
-            name: "Showreel",
-            img: "img/3.jpeg",
-            video: "https://www.youtube.com/embed/DkB2FKEP274"
-        },
+// Remove bottom borders from all tab items
+function removeBorder() {
+	tabItems.forEach(item => {
+		item.classList.remove('tab-border');
+	});
+}
 
-        {
-            name: "Showreel",
-            img: "img/4.jpeg",
-            video: "https://www.youtube.com/embed/lUr8-Y2aae4"
-        },
+// Remove show class from all content items
+function removeShow() {
+	tabContentItems.forEach(item => {
+		item.classList.remove('show');
+	});
+}
 
-        {
-            name: "Showreel",
-            img: "img/5.jpeg",
-            video: "https://www.youtube.com/embed/JG5DNuh-1tY"
-        },
-
-        {
-            name: "Showreel",
-            img: "img/6.jpeg",
-            video: "https://www.youtube.com/embed/fkBXO1RfuOw"
-        },
-
-        {
-            name: "Showreel",
-            img: "img/7.jpeg",
-            video: "https://www.youtube.com/embed/WXUIUe6NSFA"
-        },
-
-        {
-            name: "Showreel",
-            img: "img/8.jpeg",
-            video: "https://www.youtube.com/embed/as0MMwCR0NQ"
-        },
-
-        {
-            name: "Showreel",
-            img: "img/9.jpeg",
-            video: "https://www.youtube.com/embed/e88UnsczUws"
-        },
-
-        {
-            name: "Showreel",
-            img: "img/10.jpeg",
-            video: "https://www.youtube.com/embed/glJWVZ7APOM"
-        },
-
-        {
-            name: "Showreel",
-            img: "img/11.jpeg",
-            video: "https://www.youtube.com/embed/Fgv-Y8ITDNg"
-        },
-
-        {
-            name: "Showreel",
-            img: "img/12.jpeg",
-            video: "https://www.youtube.com/embed/DG6eb4jCFmU"
-        },
-
-        {
-            name: "Showreel",
-            img: "img/13.jpeg",
-            video: "https://www.youtube.com/embed/HAPCvCK2fCY"
-        },
-        
-        
-        
-
-
-    ];
-
-    const videosContent = document.querySelector('.videos__content');
-
-    videos.forEach(function (el) {
-        const template = `
-            <div class="videos__item" data-video="${el.video}">
-            <p class="videos__name">${el.name}</p> 
-            <i class="videos__play"> <img src="https://www.rafaelalucas.com/dailyui/16/assets/play.svg" alt = "" ></i>
-            <img class="videos__image" src="${el.img}" alt = "" >
-            </div>`;
-        videosContent.insertAdjacentHTML('beforeend', template);
-
-    });
-
-
-    const videoItem = document.querySelectorAll('.videos__item'),
-        modalVideo = document.querySelector('.modal'),
-        iconCloseVideo = document.querySelector('.modal__icon'),
-        videoFrame = document.querySelector('.modal__video iframe'),
-        body = document.querySelector('body');;
-
-    videoItem.forEach(function (el) {
-        el.addEventListener("click", openVideo);
-    })
-
-    iconCloseVideo.addEventListener("click", closeVideo);
-
-
-    function openVideo(e) {
-        const videoSrc = e.currentTarget.dataset.video;
-
-        videoFrame.src = videoSrc;
-
-        setTimeout(() => {
-            modalVideo.classList.add('open');
-
-        }, 300);
-
-        body.style.overflow = "hidden";
-
-
-    }
-
-    function closeVideo() {
-
-        modalVideo.classList.remove('open');
-        videoFrame.src = "";
-
-    }
-
-    if (window.innerWidth > 799) {
-        document.querySelector('.wrapper').style.height = window.innerHeight + "px";
-
-    }
-    document.querySelector('.modal').style.height = window.innerHeight + "px";
-
+// Listen for tab item click
+tabItems.forEach(item => {
+	item.addEventListener('click', selectItem);
 });
